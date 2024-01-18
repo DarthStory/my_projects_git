0545fbf5f230915679c4d1a197eaa5cce034cd7d
DarthStory <david.owen@csuglobal.edu>
2024-01-18 07:37:09 -0600

    Week 1 project complete.

---
+++ Week1/src/Week1.java
@@ -0,0 +1,20 @@
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		String firstName= "Alex";
+		String lastName = "Dragon";
+		String streetAddress = "123 Somewhere Dr.";
+		String cityName = "Utopia";
+		String zipcode = "90210";
+				
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(cityName);
+		System.out.println(zipcode);
+		
+		
+	}
+
+}
