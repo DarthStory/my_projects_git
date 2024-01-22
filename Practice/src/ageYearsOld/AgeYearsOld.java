301dde1b252fc8bf9221e5467a25c33f5bc39236
DarthStory <david.owen@csuglobal.edu>
2024-01-21 20:45:51 -0600

    Added ageYearsOld Practice

---
+++ Practice/src/ageYearsOld/AgeYearsOld.java
@@ -0,0 +1,23 @@
+package ageYearsOld;
+
+import java.util.Scanner;
+
+public class AgeYearsOld {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userAgeYears;
+      int userAgeDays;
+      int userAgeMinutes;
+      
+      System.out.print("Enter your age in years: ");
+      userAgeYears = scnr.nextInt();
+      
+      userAgeDays = userAgeYears * 365;               // Calculate days without leap years
+      userAgeDays = userAgeDays + (userAgeYears / 4); // Add days for leap years
+   
+      System.out.println("You are " + userAgeDays + " days old.");
+   
+      userAgeMinutes = userAgeDays * 24 * 60;         // 24 hours/day, 60 minutes/hour
+      System.out.println("You are " + userAgeMinutes + " minutes old.");
+   }
+}
--- Practice/src/inputFromUser/InputFromUser.java
+++ Practice/src/inputFromUser/InputFromUser.java
@@ -11,7 +11,5 @@ public class InputFromUser {
 		String greeting = "Hello, " + name + "!";
 		System.out.println(greeting);
 		
-
 	}
-
 }
