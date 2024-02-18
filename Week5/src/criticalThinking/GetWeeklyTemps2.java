8f8d2d9589b05c471c86ea8b6e8d89977a4c40bb
DarthStory <david.owen@csuglobal.edu>
2024-02-18 08:50:25 -0600

    added try/catch for sting entered instead of int 2

--- Week5/src/criticalThinking/GetWeeklyTemps2.java
+++ Week5/src/criticalThinking/GetWeeklyTemps2.java
@@ -3,7 +3,6 @@ package criticalThinking;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.ListIterator;
-import java.util.NoSuchElementException;
 import java.util.InputMismatchException;
 
 public class GetWeeklyTemps2 {
@@ -19,53 +18,52 @@ public class GetWeeklyTemps2 {
 		while (i < 12) {
 			System.out.println("What Month is it?");
 			String answer = scnr.next();
-				if (!months.contains(answer)) {
-					months.add(answer);
-					System.out.println("What was the average temp of that month?");
-					try {
-					Integer temp = scnr.nextInt();
-					averageTemps.add(temp);
-					total += temp;
-						if (temp < minimum) {
+			if (!months.contains(answer)) {
+				months.add(answer);
+				System.out.println("What was the average temp of that month?");
+				try {
+				Integer temp = scnr.nextInt();
+				averageTemps.add(temp);
+				total += temp;
+					if (temp < minimum) {
 							minimum = temp;
-						}
-						if (temp > maximum) {
-							maximum = temp;
-						}
-					++i;
-					}catch (InputMismatchException e) {
-						System.out.println("Put in an average degree for the month.");
 					}
+					if (temp > maximum) {
+							maximum = temp;
+					}
+				++i;
+				}catch (InputMismatchException e) {
+					System.out.println("Put in an average degree for the month.");
 				}
-				else {
-					System.out.println("You already added that month.");
-				}
-			
+			}
+			else {
+				System.out.println("You already added that month.");
+			}
 		}
+		
 		String mon;
 		System.out.println("If you want the a specific Month's average, type that month."
 				+ " If you want the whole year, with average, Min/Max temp, type year");
 		mon = scnr.next();
 		int index = months.indexOf(mon);
-			if (months.contains(mon)) {
+		if (months.contains(mon)) {
 			System.out.println(mon + " average of " + averageTemps.get(index) + " degrees.");
+		}
+		else if (mon.equals("year")) {
+			ListIterator<String> iterator = months.listIterator();
+			ListIterator<Integer> iterators = averageTemps.listIterator();
+			int j = 0;
+			while (j < 12) {
+				System.out.println(iterator.next() + " was an average"
+						+ " of " + iterators.next() + " degrees.");
+				++j;
 			}
-			else if (mon.equals("year")) {
-				ListIterator<String> iterator = months.listIterator();
-				ListIterator<Integer> iterators = averageTemps.listIterator();
-				int j = 0;
-				while (j < 12) {
-					System.out.println(iterator.next() + " was an average"
-							+ " of " + iterators.next() + " degrees.");
-					++j;
-				}
-				System.out.println("Maximum temp for the year was: " + maximum);
-				System.out.println("Minimum temp for the year was: " + minimum);
-				System.out.println("Average temp for the whole year was: " + total / 12);
-
+			System.out.println("Maximum temp for the year was: " + maximum);
+			System.out.println("Minimum temp for the year was: " + minimum);
+			System.out.println("Average temp for the whole year was: " + total / 12);
 			}
-			else {
+		else {
 				System.out.println("Isn't Chicago weather something??");
-			}		
+		}		
 	}		
 }
