019cc38a13ae8c0f661e6a739440d263e60bb81a
DarthStory <david.owen@csuglobal.edu>
2024-02-21 20:50:35 -0600

    Added final

---
+++ FinalCSC320/src/dealershipInventory/InteractiveDealershipInventory.java
@@ -0,0 +1,10 @@
+package dealershipInventory;
+
+public class InteractiveDealershipInventory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Week6/src/lessons/Player.java
+++ Week6/src/lessons/Player.java
@@ -3,16 +3,16 @@ package lessons;
 public class Player {
 	
 	 public static void printPoints(String name, int age, int totalPoints) {
-	      System.out.println(name + " is " + age);
-	      System.out.println(name + " made " + totalPoints + " points");
-	   }
+	     System.out.println(name + " is " + age);
+	     System.out.println(name + " made " + totalPoints + " points");
+	 }
 	   
-	   public static void main (String [] args) {
-	      String userName = "Ron";
-	      int userAge = 18;
-	      int regularTimePoints = 25;
-	      int overtimePoints = 6;
+	 public static void main (String [] args) {
+		 String userName = "Ron";
+	     int userAge = 18;
+	     int regularTimePoints = 25;
+	     int overtimePoints = 6;
 	      
-	      printPoints(userName, userAge, regularTimePoints + overtimePoints);
-	   }
-	}
+	     printPoints(userName, userAge, regularTimePoints + overtimePoints);
+	 }
+}
