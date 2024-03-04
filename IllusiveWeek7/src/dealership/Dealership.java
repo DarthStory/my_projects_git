8287186220d936e2c47ad16df4bc0b9b46dd2dc0
DarthStory <david.owen@csuglobal.edu>
2024-03-04 13:31:36 -0600

    made more readable

--- IllusiveWeek7/src/dealership/Dealership.java
+++ IllusiveWeek7/src/dealership/Dealership.java
@@ -18,9 +18,14 @@ public class Dealership {
 			System.out.println("4. View all automobiles");
 			System.out.println("0. Exit Inventory");
 			
-			System.out.print("Choose option: ");
-			int choice = scnr.nextInt();
-			
+			System.out.print("Choose option 0-4.");
+			int choice = 0;
+			try {
+			choice = scnr.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid entry. Please try again. 0-4: ");
+				scnr.nextInt();
+			}
 			switch (choice) {
 			case 0:
 				System.out.println("Exiting application.");
@@ -28,79 +33,15 @@ public class Dealership {
 				return;
 			case 1:
 				// add 
-				System.out.println("Enter Make, Model, Color, Year, and Mileage");
-				String make = scnr.next();
-				String model = scnr.next();
-				String color = scnr.next();
-				
-				int year = 0;
-				while(year == 0) {
-					try {
-						year = scnr.nextInt();
-					} catch (InputMismatchException e) {
-						System.out.println("Invalid year. Please enter again.");
-					}
-				}
-				int mileage = 0;
-				while (mileage == 0) {
-					try {
-						mileage = scnr.nextInt();
-					} catch (InputMismatchException e) {
-						System.out.println("Invalid mileage. Please enter again.");
-					}
-				}
-				autoInventory.addAutomobile(make, model, color, year, mileage);
-				System.out.println("Automobile added");
+				autoInventory.autoAddPrompt();
 				break;
 			case 2:
 				// Modify an entry
-				System.out.print("Enter the number of the automobile to modify: ");
-				int modifyIndex = scnr.nextInt();
-				
-				
-				if(modifyIndex <= 0 || modifyIndex > autoInventory.getAutomobiles().size()) {
-					System.out.println("Invalid number. Please try again.");
-					break;
-				}
-				
-				System.out.println("Enter a new automobile Make, Model, Color, Year, and Mileage.");
-				String newMake = scnr.next();
-				String newModel = scnr.next();
-				String newColor = scnr.next();
-			
-				int newYear = 0;
-				while (newYear == 0) {
-					try {
-						newYear = scnr.nextInt();
-					} catch (InputMismatchException e) {
-						System.out.println("Invalid year. Please enter again.");
-						scnr.next();
-					}
-				}
-				int newMileage = 0;
-				while (newMileage == 0) {
-					try {
-						newMileage = scnr.nextInt();
-					} catch (InputMismatchException e) {
-						System.out.println("Invalid year. Please enter again.");
-						scnr.next();
-					}
-				}
-				autoInventory.editAutomobile(modifyIndex - 1, newMake, newModel, newColor, newYear, newMileage);
-				System.out.println("Automobile modified successfuly.");
+				autoInventory.autoModifyPrompt();
 				break;			
 			case 3:
 				// delete
-				System.out.println("Enter the number of the automobile to delete: ");
-				int deleteIndex = scnr.nextInt();
-				scnr.nextLine();
-				
-				if (deleteIndex <= 0 || deleteIndex > autoInventory.getAutomobiles().size()) {
-					System.out.println("Invalid number. Please try again.");
-					break;
-				}
-				autoInventory.deleteAutomobile(deleteIndex - 1);
-				System.out.println("Automobile deleted successfully.");
+				autoInventory.autoDeletePropt();
 				break;
 			case 4:
 				// View all automobiles
