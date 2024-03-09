ea2700acfea37d8d8e2b77401f0b74db84b4c828
DarthStory <david.owen@csuglobal.edu>
2024-03-08 22:03:09 -0600

    git practice for branches and merging

---
+++ Practice/src/dealership/AutoInventory.java
@@ -0,0 +1,63 @@
+package dealership;
+
+// import statements to get utilities ArrayList and List
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoInventory {
+
+		// creates a list of automobile called automobiles
+		private List<Automobile> automobiles;
+		
+		//Constructor
+		public AutoInventory() {
+			// names the arrayList (automobiles)
+			automobiles = new ArrayList<>();
+		}
+		// Add Automobile
+		// method with variables that were input from the user
+		public void addAutomobile(String make, String model, String color, int year, int mileage) {
+			// class object new object (variables of that object)
+			Automobile automobile = new Automobile(make, model, color, year, mileage);
+			// adds object to list
+			automobiles.add(automobile);
+		}
+		// Edit an Automobile
+		public void editAutomobile(int index, String make, String model, String color, int year, int mileage) {
+			// Class object array get index
+			Automobile automobile = automobiles.get(index);
+			// modification to make to each variable
+			automobile.setMake(make);
+			automobile.setModel(model);
+			automobile.setColor(color);
+			automobile.setYear(year);
+			automobile.setMileage(mileage);
+		}
+		// get method with the list of automobile that returns the array (automobiles)
+		public List<Automobile> getAutomobiles() {
+			return automobiles;
+		}
+		
+		// view all Automobiles
+		public void viewAutomobiles() {
+			// if nothing in the list automobiles
+			if (automobiles.isEmpty()) {
+				// state list is empty
+				System.out.println("No automobiles found.");
+				return;
+			}
+			// setups loop to progress through the list
+			for(int i = 0; i < automobiles.size(); i++) {
+				// prints out the index of the object, and the variables of that object (automobile)
+				System.out.print("Automobile #" + (i + 1) + " ");
+				//prints out the whole list (automobiles)
+				System.out.println(automobiles.get(i));
+			}
+		}
+		
+		// Delete an automobile
+		// takes index and deletes the variables associated with that index
+		public void deleteAutomobile(int index) {
+			automobiles.remove(index);
+		}
+}
